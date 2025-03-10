ded38028498b1d7ee9efc69d26cc285a5e15be45
Andrey Boyko <inboxru@yandex.ru>
2025-03-10 14:22:56 +0300

    Input menu validation

--- src/main/java/ru/ftptpf/AppRunner.java
+++ src/main/java/ru/ftptpf/AppRunner.java
@@ -22,47 +22,52 @@ public class AppRunner {
             System.out.println("5 - Выйти из программы");
             System.out.println("Введите числовой номер меню: ");
 
-            int choice = scanner.nextInt();
+            if (!scanner.hasNextInt()) {
+                System.out.println("Необходимо ввести числовой номер меню. Это цифры от 1 до 5. Попробуйте еще раз.");
+                scanner.nextLine();
+            } else {
+                int choice = scanner.nextInt();
 
-            switch (choice) {
-                case 1:
-                    PdfService merge = new Merge();
-                    merge.run();
-                    break;
-                case 2:
-                    System.out.println("Введите имя файла (включая расширение *.pdf, например 12345.pdf) в который будут вставляться страницы другого pdf файла.");
-                    String mainFileName = scanner.next();
-                    System.out.println("Введите имя файла (включая расширение *.pdf, например 45.pdf) из которого будут извлекаться страницы для вставки.");
-                    String addedFileName = scanner.next();
-                    System.out.println("Введите номер страницы основного pdf файла, после которой начнется вставка.");
-                    int insertAfterThisPage = scanner.nextInt();
-                    PdfService insert = new Insert(mainFileName, addedFileName, insertAfterThisPage);
-                    insert.run();
-                    break;
-                case 3:
-                    System.out.println("Необходимо указать диапазон извлекаемых страниц.");
-                    System.out.println("Введите номер первой страницы:");
-                    int startPage = scanner.nextInt();
-                    System.out.println("Введите номер последней страницы:");
-                    int endPage = scanner.nextInt();
-                    PdfService extract = new Extract(startPage, endPage);
-                    extract.run();
-                    break;
-                case 4:
-                    System.out.println("Необходимо указать диапазон удаляемых страниц.");
-                    System.out.println("Введите номер первой страницы:");
-                    int firstPage = scanner.nextInt();
-                    System.out.println("Введите номер последней страницы:");
-                    int lastPage = scanner.nextInt();
-                    PdfService delete = new Delete(firstPage, lastPage);
-                    delete.run();
-                    break;
-                case 5:
-                    exit = true;
-                    System.out.println("Работа с программой завершена.");
-                    break;
-                default:
-                    System.out.println("Неверно выбран номер меню");
+                switch (choice) {
+                    case 1:
+                        PdfService merge = new Merge();
+                        merge.run();
+                        break;
+                    case 2:
+                        System.out.println("Введите имя файла (включая расширение *.pdf, например 12345.pdf) в который будут вставляться страницы другого pdf файла.");
+                        String mainFileName = scanner.next();
+                        System.out.println("Введите имя файла (включая расширение *.pdf, например 45.pdf) из которого будут извлекаться страницы для вставки.");
+                        String addedFileName = scanner.next();
+                        System.out.println("Введите номер страницы основного pdf файла, после которой начнется вставка.");
+                        int insertAfterThisPage = scanner.nextInt();
+                        PdfService insert = new Insert(mainFileName, addedFileName, insertAfterThisPage);
+                        insert.run();
+                        break;
+                    case 3:
+                        System.out.println("Необходимо указать диапазон извлекаемых страниц.");
+                        System.out.println("Введите номер первой страницы:");
+                        int startPage = scanner.nextInt();
+                        System.out.println("Введите номер последней страницы:");
+                        int endPage = scanner.nextInt();
+                        PdfService extract = new Extract(startPage, endPage);
+                        extract.run();
+                        break;
+                    case 4:
+                        System.out.println("Необходимо указать диапазон удаляемых страниц.");
+                        System.out.println("Введите номер первой страницы:");
+                        int firstPage = scanner.nextInt();
+                        System.out.println("Введите номер последней страницы:");
+                        int lastPage = scanner.nextInt();
+                        PdfService delete = new Delete(firstPage, lastPage);
+                        delete.run();
+                        break;
+                    case 5:
+                        exit = true;
+                        System.out.println("Работа с программой завершена.");
+                        break;
+                    default:
+                        System.out.println("Неверно выбран номер меню");
+                }
             }
         }
     }
